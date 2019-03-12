8798f90df4dbc6d0f61b41b83b030e03792c2447
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-03-12 22:40:16 +0300

    Create RestExceptionHandler, which handling ResourceNotFoundException
    Signed-off-by: Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>

--- src/main/java/com/danizz/manageuserserver/domain/exception/ResourceNotFoundException.java
+++ src/main/java/com/danizz/manageuserserver/domain/exception/ResourceNotFoundException.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+//@ResponseStatus(value = HttpStatus.NOT_FOUND)
 @Getter
 @Setter
 public class ResourceNotFoundException extends RuntimeException {
